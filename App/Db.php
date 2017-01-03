2a1e536dac2e096bb25cf377a7aa72906a270712
telenord <nordtelecom@yandex.ru>
2017-01-03 16:43:38 +0300

    Db query

--- App/Db.php
+++ App/Db.php
@@ -14,4 +14,14 @@
         $res = $sth->execute();
         return $res;
     }
+
+     public function query($sql){
+         $sth = $this->dbh->prepare($sql);
+         $res = $sth->execute();
+         if(false !== $res){
+             return $sth->fetchAll();
+         }
+         return array();
+     }
+
  }
--- index.php
+++ index.php
@@ -3,5 +3,5 @@
  require __DIR__ . '/autoload.php';
 
  $db = new \App\Db();
- $res = $db->execute('CREATE TABLE news (id SERIAL)');
- var_dump($res);
+ $data = $db->query('SELECT * FROM news2');
+ var_dump($data);
