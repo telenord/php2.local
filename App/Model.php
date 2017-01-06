398c58c671365cedfc4da3d282ebc33a6817b051
telenord <nordtelecom@yandex.ru>
2017-01-06 14:18:44 +0300

    Singleton

--- App/Model.php
+++ App/Model.php
@@ -7,6 +7,8 @@ namespace App;
 abstract class Model
 {
     const TABLE='';
+    public $id;
+
     public static function findAll(){
         $db = new Db();
         return $db->query('SELECT * FROM'. static::TABLE, static::$class);
@@ -15,5 +17,26 @@ abstract class Model
     public static function findById(int $id){
         $db = new Db();
         return echo ('ID =' . $id); //$db->query('SELECT $id FROM'. static::TABLE, static::class);
+    }
+
+    public  function isNew(){
+        return empty($this->id);
+
+    }
+    public function insert(){
+        if( !$this->isNew())
+        {
+            return;
+        }
+            $db = Db::instance();
+
+            foreach($values as $k->$v){
+
+            }
+            $sql = "INSERT INTO ".static::TABLE . '';
+
+
+
+
     }
 }
